dccf9e0e8640531932132cb1c1d321b654ed0270
Yamuna_Dinesh <85825883+yamuna1992@users.noreply.github.com>
2021-06-27 21:45:30 +0530

    Add files via upload

---
+++ Inheritance_Computer.java
@@ -0,0 +1,14 @@
+package Week3.Day1;
+
+public class Inheritance_Computer {
+
+	public void computerModel() {
+		System.out.println("Computer Model");
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
