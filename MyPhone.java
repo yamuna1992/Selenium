53420a81cd4fcdfdf036590aa06e9ef61673de46
Yamuna_Dinesh <85825883+yamuna1992@users.noreply.github.com>
2021-06-27 21:47:34 +0530

    Add files via upload

---
+++ MyPhone.java
@@ -0,0 +1,14 @@
+package Week3.Day1;
+
+public class MyPhone {
+
+	public static void main(String[] args) {
+		
+		SmartPhone sp = new SmartPhone();
+		sp.connectWhatsApp();
+		sp.makeCall();
+		sp.saveContact();
+		sp.sendMsg();
+		SmartPhone.takeVideo();
+	}
+}
