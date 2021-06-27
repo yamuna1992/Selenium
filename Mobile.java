d56305910c66d6de441d054117e6acc0c1fa8fe7
Yamuna_Dinesh <85825883+yamuna1992@users.noreply.github.com>
2021-06-27 21:46:04 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,16 @@
+package Week3.Day1;
+
+public class Mobile {
+
+	public static void sendMsg() {
+		System.out.println("Message Sent");
+	}
+
+	public static void makeCall() {
+		System.out.println("making call");
+	}
+
+	public static void saveContact() {
+		System.out.println("saved contact");
+	}
+}
