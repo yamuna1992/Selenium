bd569f2e7e951778a05d63467ed9cb41ce4805a1
Yamuna_Dinesh <85825883+yamuna1992@users.noreply.github.com>
2021-06-27 21:46:27 +0530

    Add files via upload

---
+++ Multilevel_College.java
@@ -0,0 +1,17 @@
+package Week3.Day1;
+
+public class Multilevel_College {
+
+	public void collegeName() {
+		System.out.println("College Name: Karpaga Vinayaga College");
+	}
+
+	public void collegeCode() {
+		System.out.println("College Code: KA123");
+	}
+
+	public void collegeRank() {
+		System.out.println("College Rank: 5th Rank");
+
+	}
+}
