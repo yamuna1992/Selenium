fab50b11cc4fcdfaea288d30a79d80b2ea0b08e0
Yamuna_Dinesh <85825883+yamuna1992@users.noreply.github.com>
2021-06-27 21:48:12 +0530

    Add files via upload

---
+++ Override_BankInfo.java
@@ -0,0 +1,19 @@
+package Week3.Day1;
+
+public class Override_BankInfo {
+
+	public void saving() {
+		System.out.println("Saving Account");
+
+	}
+
+	public void fixed() {
+		System.out.println("Fixed Account");
+
+	}
+
+	public void deposit() {
+
+		System.out.println("Deposite Account");
+	}
+}
