902ff33036f9a352f819e54e2c1ab0d075429d96
Yamuna_Dinesh <85825883+yamuna1992@users.noreply.github.com>
2021-06-28 10:41:01 +0530

    Add files via upload

---
+++ Assig4_MoveAll0ToRight.java
@@ -0,0 +1,31 @@
+package Week3.Day2;
+
+public class Assig4_MoveAll0ToRight {
+
+	public static void main(String[] args) {
+
+		int[] array_nums = { 4, 0, 3, 0, 1, 5, 2, 0 };
+		int i = 0;
+		System.out.print("\nOriginal array: \n");
+		for (int n : array_nums)
+			System.out.print(n + "  ");
+
+		for (int j = 0, l = array_nums.length; j < l;) {
+			if (array_nums[j] == 0)
+				j++;
+			else {
+				int temp = array_nums[i];
+				array_nums[i] = array_nums[j];
+				array_nums[j] = temp;
+				i++;
+				j++;
+			}
+		}
+		while (i < array_nums.length)
+			array_nums[i++] = 0;
+		System.out.print("\nAfter moving 0's to the end of the array: \n");
+		for (int n : array_nums)
+			System.out.print(n + "  ");
+		System.out.print("\n");
+	}
+}
